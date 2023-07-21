13f166ae2d02e02b01fff2335600d9351ccdcd1b
facuricher <facu.richer@gmail.com>
2023-07-20 22:42:54 -0300

    haciendo prueba de test

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -14,6 +14,7 @@ export function Home() {
  return(
   <>
       <h1>Find your favorite gifs</h1>
+      <h2>esto es una prueba</h2>
       <AddCategory 
         onNewCateory={ addCategory }
       />
